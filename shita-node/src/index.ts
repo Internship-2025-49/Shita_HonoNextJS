5aaa5cfb751eeec3a5310cfde5820df7c2041aea
Shita Zeny <shita0639@gmail.com>
2025-02-11 22:13:21 +0700

    feat: add CORS middleware to enable cross-origin requests #2

--- shita-node/src/index.ts
+++ shita-node/src/index.ts
@@ -2,8 +2,16 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { Routes } from './routes/index.js'
 import { handle } from 'hono/vercel';
+import { cors } from 'hono/cors';
 
 const app = new Hono().basePath('/api')
+
+app.use('*', cors({
+  origin: 'http://localhost:3000',
+  allowMethods: ['GET','POST', 'PUT', 'DELETE'], 
+  allowHeaders: ["*"],
+}));
+
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
